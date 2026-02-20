35ad1a03499089946cfefda11a8f1829a28914ac
parneetsingh022 <parneetsingh022@gmail.com>
2026-02-20 14:53:54 -0800

    feat: update opcodes and runner to support new CHAR value type and enhance output formatting

--- src/vm/opcodes.rs
+++ src/vm/opcodes.rs
@@ -1,39 +1,17 @@
-macro_rules! define_opcodes {
-    // This part handles the recursion to assign numbers
-    (@count $asm:ident, $output:ident, $ip:ident, $bytecode:ident, $val:expr, ) => {};
-    
-    ($($name:ident),*) => {
-        pub mod op {
-            // Internal trick to generate incrementing constants
-            $(pub const $name: u8 = define_opcodes!(@inner $name);)*
-            
-            // Generate the lookup function
-            pub fn get_name(code: u8) -> &'static str {
-                let mut i = 0;
-                $(
-                    if code == i { return stringify!($name); }
-                    i += 1;
-                )*
-                "UNKNOWN"
-            }
-        }
-    };
-}
-
 
 macro_rules! opcodes {
     ($($name:ident),*) => {
         pub mod op {
-            $(pub const $name: u8 = op_enum::$name as u8;)*
+            $(pub const $name: u8 = opEnum::$name as u8;)*
 
             #[repr(u8)]
             #[derive(Debug)]
-            enum op_enum {
+            enum opEnum {
                 $($name,)*
             }
 
             pub fn get_name(code: u8) -> &'static str {
-                $(if code == op_enum::$name as u8 { return stringify!($name); })*
+                $(if code == opEnum::$name as u8 { return stringify!($name); })*
                 "UNKNOWN"
             }
         }
